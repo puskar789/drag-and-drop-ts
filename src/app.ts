90e5d4761d6279a230526807970a86622b039626
puskar789 <pbohara231159@gmail.com>
2025-02-25 22:14:10 +0530

    modified app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 
-// renders the app
+// renders the form and lists
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
